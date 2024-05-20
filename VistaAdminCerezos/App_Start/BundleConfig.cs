980f16c313694c26afd60495a6a168f852434714
MARLON GARC�A SEP�LVEDA <marlongarcia299859@correo.itm.edu.co>
2024-05-20 17:13:05 -0500

    Instalo componente datatable en el proyecto, configuro estilos y js del datatable en bundleconfigs.cs, quite rol de la columna documentado en d_Usuarios vista de datos, Tambien en Usuarios.cshtml. No me lee los css del BundleConfig asi que los puse en Usuarios para solucionar el estilo de la tabla.

--- VistaAdminCerezos/App_Start/BundleConfig.cs
+++ VistaAdminCerezos/App_Start/BundleConfig.cs
@@ -5,6 +5,7 @@ namespace VistaAdminCerezos
 {
     public class BundleConfig
     {
+        //REVISAR CONEXIONES
         // Para obtener más información sobre las uniones, visite https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {

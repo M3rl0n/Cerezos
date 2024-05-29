2b01316b4ca12e2ac402870c268d5611c616e320
MARLON GARC�A SEP�LVEDA <marlongarcia299859@correo.itm.edu.co>
2024-05-29 18:04:52 -0500

    Final presentar mañana

---
+++ VistaEntidad/Paypal/Response_Paypal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VistaEntidad.Paypal
+{
+    public class Response_Paypal<T>
+    {
+        //Clase generica permite response capture y response cheackout en una sola clase
+        public bool Status { get; set; }
+        public T Response { get; set; }
+    }
+
+}

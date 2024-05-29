eb0861c77f36639687b44bb8e28f55b4ef5ec174
MARLON GARC�A SEP�LVEDA <marlongarcia299859@correo.itm.edu.co>
2024-05-29 01:15:13 -0500

    Se realiza actualizacion general del proyecto

--- VistaTiendaCerezos/Controllers/TiendaCerezosController.cs
+++ VistaTiendaCerezos/Controllers/TiendaCerezosController.cs
@@ -9,8 +9,10 @@ using VistaNegocio;
 
 namespace VistaTiendaCerezos.Controllers
 {
+
     public class TiendaCerezosController : Controller
     {
+        private const int IDClienteTest = 1;
         // GET: TiendaCerezos
         public ActionResult Index()
         {
@@ -79,6 +81,135 @@ namespace VistaTiendaCerezos.Controllers
 
         }
 
+        //Agregar al carrito
+
+        [HttpPost]
+        public JsonResult AgregarCarrito(int IDProducto)
+        {
+            bool existe = new N_CarritoCompras().ExisteCarrito(IDProducto);
+            bool respuesta = false;
+
+            string mensaje = string.Empty;
+
+            if (existe)
+            {
+                mensaje = "El producto ya existe en el carrito";
+            }
+            else
+            {
+                respuesta = new N_CarritoCompras().OperacionCarrito(IDProducto, true, out mensaje);
+            }
+
+            return Json(new { respuesta = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+        }
+
+        //Devolver cantidad de producto segun el carrito
+
+        [HttpGet]
+
+        public JsonResult CantidadEnCarrito()
+        {
+            int? IDCliente = null;
+            int cantidad = new N_CarritoCompras().CantidadEnCarrito(IDCliente);
+            return Json(new {cantidad = cantidad}, JsonRequestBehavior.AllowGet);
+        }
+
+        //Devolver lista de prodcutos 
+
+        [HttpPost]
+        
+        public JsonResult ListarProductosCarrito()
+        {
+            List<CarritoCerezos> olista = new List<CarritoCerezos>();
+            bool conversion;
+
+            olista = new N_CarritoCompras().listarProducto(IDClienteTest).Select(oc => new CarritoCerezos()
+            {
+                oProducto = new ProductosCerezos()
+                {
+                    IDProducto = oc.oProducto.IDProducto,
+                    Nombre = oc.oProducto.Nombre,
+                    oCategoria = oc.oProducto.oCategoria,
+                    Precio = oc.oProducto.Precio,
+                    RutaImagen = oc.oProducto.RutaImagen,
+                    Base64 = N_Recursos.ConvertirBase64(Path.Combine(oc.oProducto.RutaImagen, oc.oProducto.NombreImagen), out conversion),
+                    Extension = Path.GetExtension(oc.oProducto.NombreImagen)
+                },
+                Cantidad = oc.Cantidad
+            }).ToList();
+
+            return Json(new { data = olista }, JsonRequestBehavior.AllowGet);
+
+        }
+
+
+        //Calcular carrito de compras
+
+        [HttpPost]
+        public JsonResult OperacionCarrito(int IDProducto, bool sumar)
+        {
+
+            bool respuesta = false;
+
+            string mensaje = string.Empty;
+
+            respuesta = new N_CarritoCompras().OperacionCarrito(IDProducto, true, out mensaje);
+
+            return Json(new { respuesta = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+           
+        }
+
+        //eliminar producto dentro del carrito
+
+        [HttpPost]
+
+        public JsonResult EliminarCarrito(int IDProducto)
+        {
+            bool respuesta = false;
+
+            string mensaje = string.Empty;
+
+            respuesta = new N_CarritoCompras().EliminarCarrito(IDProducto);
+
+            return Json(new { respuesta = respuesta, mensaje = mensaje }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        //Obter departamentos
+
+        [HttpPost]
+
+        public JsonResult ObtenerDepartamentos()
+        {
+            List<DepartamentoCerezos> oLista = new List<DepartamentoCerezos>();
+
+            oLista = new N_Ubicacion().Departamentos();
+
+            return Json(new {lista = oLista}, JsonRequestBehavior.AllowGet);
+
+        }
+
+        //Obter Ciudades
+
+        [HttpPost]
+
+        public JsonResult ObtenerCiudades()
+        {
+            List<Ciudad> oLista = new List<Ciudad>();
+
+            oLista = new N_Ubicacion().Ciudades();
+
+            return Json(new { lista = oLista }, JsonRequestBehavior.AllowGet);
+
+        }
+
+        //vista de carrito
+
+        public ActionResult Carrito()
+        {
+            return View();
+        }
 
     }
 }
